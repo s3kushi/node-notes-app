6af9faa9b96a14a98e66ee061141dbb55a509a3d
s3kushi <daniel.756@hotmail.com>
2021-01-03 20:55:17 -0300

    Added methods to fetch data from the weather and geolocation APIs

--- simple-weather-app/src/app.js
+++ simple-weather-app/src/app.js
@@ -1,1 +1,36 @@
-const request = require('postman-request')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+const inputLocation = process.argv[2]
+
+if(!inputLocation) return console.error('Location parameter missing')
+
+callbackMethod = () => {
+    geocode.geocodeCallback(inputLocation, (error, { latitude, longitude, location } = {}) => {
+        if(error) {
+            return console.log(error)
+        }
+    
+        forecast.forecastCallback(latitude, longitude, (error, forecastData) => {
+            if(error) {
+                return console.log(error)
+            }
+            console.log('Location ', location)
+            console.log('Forecast ', forecastData)
+        })
+    })
+}
+
+asyncMethod = async () => {
+    try {
+        const { latitude, longitude, location } = await geocode.geocodePromise(inputLocation)
+        const forecastData = await forecast.forecastPromise(latitude, longitude)
+
+        console.log('Location ', location)
+        console.log('Forecast ', forecastData)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+//callbackMethod()
+//asyncMethod()
---
+++ simple-weather-app/src/utils/forecast.js
@@ -0,0 +1,43 @@
+const request = require('postman-request')
+
+const forecastPromise = (latitude, longitude) => {
+    const url = `http://api.weatherstack.com/current?access_key=d674ffd19a5038c601fe203227d2c081&query=${latitude},${longitude}&units=m`
+
+    return new Promise((resolve, reject) => {
+        request({ url, json: true }, (error, { body } = {}) => {
+            if(error) {
+                reject('Unable to connect to weather service')
+                return
+            }
+            if(body.error) {
+                reject('Unable to find location')
+                return
+            }
+            resolve(`${body.current.weather_descriptions[0]} It is currently ${body.current.temperature} degrees out. 
+                    It feels like ${body.current.feelslike} degrees out.`)
+        })
+    })
+}
+
+const forecastCallback = (latitude, longitude, callback) => {
+    const url = `http://api.weatherstack.com/current?access_key=d674ffd19a5038c601fe203227d2c081&query=${latitude},${longitude}&units=m`
+
+    request({ url, json: true }, (error, { body } = {}) => {
+        if(error) {
+            callback('Unable to connect to weather service', undefined)
+            return
+        }
+        if(body.error) {
+            callback('Unable to find location', undefined)
+            return
+        }
+        callback(undefined,
+            `${body.current.weather_descriptions[0]} It is currently ${body.current.temperature} degrees out. 
+            It feels like ${body.current.feelslike} degrees out.`)
+    })
+}
+
+module.exports = {
+    forecastCallback,
+    forecastPromise
+}
